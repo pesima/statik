934034a8c460d3b7081611eebbbda073d9b2f7f2
Hariadi Hinta <diperakui@yahoo.com>
2013-02-26 12:03:54 +0800

    Load of work on gruntfile. Add grunt concat.

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,8 +27,45 @@ module.exports = function(grunt) {
           compress: true
         },
         files: {
-          '<%= dir.dest %>/assets/css/main.min.css': [ '<%= dir.dest %>/assets/js/bootstrap/less/bootstrap.less' ]
+          '<%= dir.dest %>/assets/css/bootstrap.min.css': [ '<%= dir.dest %>/assets/less/bootstrap.less' ]
         }
+      }
+    },
+    
+    concat: {
+      dev: {
+        bootstrap: {
+          '<%= dir.dest %>/assets/js/vendor/bootstrap/bootstrap.js': [ 
+            '<%= dir.dest %>/assets/js/vendor/bootstrap/bootstrap-transition.js', 
+              '<%= dir.dest %>/assets/js/vendor/bootstrap/bootstrap-alert.js', 
+              '<%= dir.dest %>/assets/js/vendor/bootstrap/bootstrap-button.js', 
+              '<%= dir.dest %>/assets/js/vendor/bootstrap/bootstrap-carousel.js', 
+              '<%= dir.dest %>/assets/js/vendor/bootstrap/bootstrap-collapse.js', 
+              '<%= dir.dest %>/assets/js/vendor/bootstrap/bootstrap-dropdown.js', 
+              '<%= dir.dest %>/assets/js/vendor/bootstrap/bootstrap-modal.js', 
+              '<%= dir.dest %>/assets/js/vendor/bootstrap/bootstrap-tooltip.js', 
+              '<%= dir.dest %>/assets/js/vendor/bootstrap/bootstrap-popover.js', 
+              '<%= dir.dest %>/assets/js/vendor/bootstrap/bootstrap-scrollspy.js', 
+              '<%= dir.dest %>/assets/js/vendor/bootstrap/bootstrap-tab.js', 
+              '<%= dir.dest %>/assets/js/vendor/bootstrap/bootstrap-typeahead.js', 
+              '<%= dir.dest %>/assets/js/vendor/bootstrap/bootstrap-affix.js'
+          ]
+        },
+        app: {
+          '<%= dir.dest %>assets/js/app.js': [ 
+            '<%= dir.dest %>/assets/js/vendor/holder.js',
+            '<%= dir.dest %>/assets/js/vendor/mustache/mustache.js',
+            '<%= dir.dest %>/assets/js/vendor/mustache/jquery.mustache.js'
+           ]
+        }
+      },
+      
+      prod: {
+        '<%= dir.dest %>/assets/js/main.js': [ 
+          '<%= dir.dest %>/assets/js/copyright.js',
+          '<%= dir.dest %>/assets/js/vendor/bootstrap/bootstrap.js',
+          '<%= dir.dest %>assets/js/app.js'
+        ]
       }
     },
     
@@ -36,21 +73,11 @@ module.exports = function(grunt) {
       dist: {
         files: {
           '<%= dir.dest %>/assets/js/scripts.min.js': [
-            '<%= dir.dest %>/assets/js/bootstrap/bootstrap-transition.js',
-            '<%= dir.dest %>/assets/js/bootstrap/bootstrap-alert.js',
-            '<%= dir.dest %>/assets/js/bootstrap/bootstrap-button.js',
-            '<%= dir.dest %>/assets/js/bootstrap/bootstrap-carousel.js',
-            '<%= dir.dest %>/assets/js/bootstrap/bootstrap-collapse.js',
-            '<%= dir.dest %>/assets/js/bootstrap/bootstrap-dropdown.js',
-            '<%= dir.dest %>/assets/js/bootstrap/bootstrap-modla.js',
-            '<%= dir.dest %>/assets/js/bootstrap/bootstrap-tooltip.js',
-            '<%= dir.dest %>/assets/js/bootstrap/bootstrap-popover.js',
-            '<%= dir.dest %>/assets/js/bootstrap/bootstrap-scrollspy.js',
-            '<%= dir.dest %>/assets/js/bootstrap/bootstrap-tab.js',
-            '<%= dir.dest %>/assets/js/bootstrap/bootstrap-typehead.js',
+            '<%= dir.dest %>/assets/js/vendor/bootstrap/bootstrap.js',
             '<%= dir.dest %>/assets/js/*.js',
             '<%= dir.dest %>/assets/js/_*.js'
           ]
+          
         }
       }
     },
@@ -79,6 +106,7 @@ module.exports = function(grunt) {
   grunt.loadTasks('tasks');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-recess');
@@ -88,6 +116,7 @@ module.exports = function(grunt) {
     'clean',
     'copy',
     'recess',
+    'concat',
     'uglify',
     'version'
   ]);
