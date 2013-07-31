df47e72836f06077a8a01352a698dfb82a1c775e
Hariadi Hinta <diperakui@yahoo.com>
2013-08-01 01:18:33 +0800

    Add Assemble task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,127 +1,50 @@
+/*
+ * Statik
+ * https://github.com/pesima/
+ *
+ * Copyright (c) 2013 PESIMA
+ * Licensed under the MIT license.
+ */
+
 'use strict';
+
 module.exports = function(grunt) {
 
+  // Project configuration.
   grunt.initConfig({
-    dir: {
-            src: 'src',
-            dest: 'dist'
-        },
-  
-    jshint: {
+    pkg: grunt.file.readJSON('package.json'),
+
+    assemble: {
+      // Task-level options
       options: {
-        jshintrc: '.jshintrc'
+        flatten: true,
+        data: 'src/**/*.{json,yml}',
+        assets: 'site/assets',
+        helpers: 'src/helpers/helper-*.js',
+        layoutdir: 'src/layouts',
+        partials: ['src/includes/**/*.hbs'],
       },
-      
-      all: [
-        'Gruntfile.js',
-        '<%= dir.src %>/assets/js/*.js',
-        '<%= dir.src %>/assets/js/*.js',
-        '!<%= dir.src %>/assets/js/scripts.min.js'
-      ]
-    },
-    
-    recess: {
-      dist: {
-        options: {
-          compile: true,
-          compress: true
-        },
-        files: {
-          '<%= dir.dest %>/assets/css/bootstrap.min.css': [ '<%= dir.dest %>/assets/less/bootstrap.less' ]
-        }
-      }
-    },
-    
-    concat: {
-      dev: {
-        bootstrap: {
-          '<%= dir.dest %>/assets/js/vendor/bootstrap/bootstrap.js': [ 
-            '<%= dir.dest %>/assets/js/vendor/bootstrap/bootstrap-transition.js', 
-              '<%= dir.dest %>/assets/js/vendor/bootstrap/bootstrap-alert.js', 
-              '<%= dir.dest %>/assets/js/vendor/bootstrap/bootstrap-button.js', 
-              '<%= dir.dest %>/assets/js/vendor/bootstrap/bootstrap-carousel.js', 
-              '<%= dir.dest %>/assets/js/vendor/bootstrap/bootstrap-collapse.js', 
-              '<%= dir.dest %>/assets/js/vendor/bootstrap/bootstrap-dropdown.js', 
-              '<%= dir.dest %>/assets/js/vendor/bootstrap/bootstrap-modal.js', 
-              '<%= dir.dest %>/assets/js/vendor/bootstrap/bootstrap-tooltip.js', 
-              '<%= dir.dest %>/assets/js/vendor/bootstrap/bootstrap-popover.js', 
-              '<%= dir.dest %>/assets/js/vendor/bootstrap/bootstrap-scrollspy.js', 
-              '<%= dir.dest %>/assets/js/vendor/bootstrap/bootstrap-tab.js', 
-              '<%= dir.dest %>/assets/js/vendor/bootstrap/bootstrap-typeahead.js', 
-              '<%= dir.dest %>/assets/js/vendor/bootstrap/bootstrap-affix.js'
-          ]
-        },
-        app: {
-          '<%= dir.dest %>assets/js/app.js': [ 
-            '<%= dir.dest %>/assets/js/vendor/holder.js',
-            '<%= dir.dest %>/assets/js/vendor/mustache/mustache.js',
-            '<%= dir.dest %>/assets/js/vendor/mustache/jquery.mustache.js'
-           ]
-        }
-      },
-      
-      prod: {
-        '<%= dir.dest %>/assets/js/main.js': [ 
-          '<%= dir.dest %>/assets/js/copyright.js',
-          '<%= dir.dest %>/assets/js/vendor/bootstrap/bootstrap.js',
-          '<%= dir.dest %>assets/js/app.js'
+      site: {
+        // Target-level options
+        options: {layout: 'default.hbs'},
+        files: [
+          { expand: true, cwd: 'src', src: ['*.hbs', '!index.hbs'], dest: 'site/' },
+          { expand: true, cwd: 'src', src: ['index.hbs'], dest: './' }
         ]
       }
     },
-    
-    uglify: {
-      dist: {
-        files: {
-          '<%= dir.dest %>/assets/js/scripts.min.js': [
-            '<%= dir.dest %>/assets/js/vendor/bootstrap/bootstrap.js',
-            '<%= dir.dest %>/assets/js/*.js',
-            '<%= dir.dest %>/assets/js/_*.js'
-          ]
-          
-        }
-      }
-    },
-    
-    watch: {
-      less: {
-        files: [ '<%= dir.src %>/assets/js/bootstrap/less/*.less' ],
-        tasks: ['copy', 'recess']
-      },
-      js: {
-        files: [
-          '<%= jshint.all %>'
-        ],
-        tasks: ['jshint']
-      }
-    },
-    
-    clean: {
-      dist: [ '<%= dir.dest %>' ]
-    }
 
-    
+    // Before generating any new files,
+    // remove any previously-created files.
+    clean: {
+      all: ['site/**/*.{html,md}', 'index.html']
+    }
   });
 
-  // Load tasks
-  grunt.loadTasks('tasks');
+  // Load npm plugins to provide necessary tasks.
+  grunt.loadNpmTasks('assemble');
   grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-recess');
 
-  // Register tasks
-  grunt.registerTask('default', [
-    'clean',
-    'copy',
-    'recess',
-    'concat',
-    'uglify',
-    'version'
-  ]);
-  grunt.registerTask('dev', [
-    'watch'
-  ]);
-
+  // Default task to be run.
+  grunt.registerTask('default', ['clean', 'assemble']);
 };
